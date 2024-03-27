43b3da35f9829198964cdde034859c1a7323879c
Victor LEONARD <victorleonard@hotmail.fr>
2024-03-27 22:38:25 +0100

    [feat] user register

---
+++ src/pages/UserRegister.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <h3>Inscription</h3>
+      <q-input
+        filled
+        v-model="username"
+        label="Votre pseudo *"
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length > 0) || 'Veillez saisir un pseudo',
+        ]"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="Mot de passe"
+        lazy-rules
+        :rules="[
+          (val) =>
+            (val !== null && val !== '') || 'Le mot de passe est obligatoire',
+        ]"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { api } from "boot/axios";
+import { useQuasar } from "quasar";
+const $q = useQuasar();
+
+const username = ref("");
+const password = ref("");
+
+function onSubmit() {
+  api
+    .post("api/users", {
+      username: username.value,
+      plainPassword: password.value,
+    })
+    .then((res) => {
+      $q.notify({
+        type: "positive",
+        message: "Vous êtes bien inscrit",
+      });
+    })
+    .catch((e) => {
+      $q.notify({
+        type: "negative",
+        message: "Echec de l'inscription",
+        icon: "report_problem",
+      });
+    });
+}
+</script>
