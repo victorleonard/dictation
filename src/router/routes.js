d527663467e01ccde4db7a111bb989b3c51115e6
Victor LEONARD <victorleonard@hotmail.fr>
2024-03-27 22:39:24 +0100

    [feat] router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,34 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/public",
+    component: () => import("layouts/PublicLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: "register",
+        component: () => import("src/pages/UserRegister.vue"),
+      },
+      {
+        path: "login",
+        component: () => import("src/pages/UserLogin.vue"),
+      },
+    ],
+  },
+  {
+    path: "/",
+    component: () => import("src/layouts/PrivateLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      { path: "new", component: () => import("pages/NewWord.vue") },
+      { path: "list", component: () => import("src/pages/WordsList.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
