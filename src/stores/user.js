2efc61f85cc5ac9d9d973e28178af0533865eb90
Victor LEONARD <victorleonard@hotmail.fr>
2024-04-24 22:36:56 +0200

    dev

--- src/stores/user.js
+++ src/stores/user.js
@@ -5,7 +5,9 @@ export const useUserStore = defineStore("user", {
   state: () => ({
     user: "",
     learnedWord: [],
+    learnedVerb: [],
     nbWords: 0,
+    nbVerbs: 0,
   }),
   getters: {},
   actions: {
@@ -17,9 +19,19 @@ export const useUserStore = defineStore("user", {
         this.learnedWord = res.data["hydra:member"];
       });
     },
+    getMyLearnedVerbs() {
+      api.get("/api/users/me/verbs").then((res) => {
+        this.learnedVerb = res.data["hydra:member"];
+      });
+    },
     getNbWords() {
       api.get("/api/words").then((res) => {
         this.nbWords = res.data["hydra:totalItems"];
+      });
+    },
+    getNbVerbs() {
+      api.get("/api/verbs").then((res) => {
+        this.nbVerbs = res.data["hydra:totalItems"];
       });
     },
   },
